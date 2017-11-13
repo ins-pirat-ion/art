3eb9fb926fd03d1dc2c7c240a90b8b9d830dcc42
Filip Krska <fkrska@pepper.brq.redhat.com>
2017-11-13 14:15:34 +0100

    HOP_TROP-Az_mi_bude_20_let_divce

---
+++ HOP_TROP-Az_mi_bude_20_let_divce.C
@@ -0,0 +1,55 @@
+HOP TROP - Až mi bude dvacet let (přechýlení ♡ 2017 Gándhí)
+                                       https://youtu.be/vvu2qyHwdR4
+Orig: E - Kapo: 4
+
+I:
+   C          Dmi7            G
+   C          Dmi7            G7          C
+1. Až mi bude dvacet let, tak koupím sobě loď,
+   Dmi7  C     F    C      Dmi7           G
+   řeknu borci jako květ: "nestůj, na loď pojď",
+     C            Dmi7           G          C
+   a když se bude zdráhati, když posedne ho strach,
+       Dmi7    C     F   C       Dmi7    G
+   tak hned ho začnu líbati v tý lodi na vlnách,
+     F       C    B     F        C          G
+   a když se bude zdráhati, když posedne ho strach,
+       C             Dmi7        G         C  Dmi7 G    C Dmi7 G
+   tak hned ho začnu líbati v tý lodi na vlnách.
+
+   C          Dmi7          G7       C  Dmi7    C     F   C      Dmi7      G
+2. Až mi bude třicet let to budu veliká všichni budou závidět mi kormidelníka
+     C           Dmi7             G          C
+   a loď už bude samej šrám, však šohaj jako květ,
+     Dmi7   C     F    C      Dmi7     G
+   a v lodi malej porcelán mi bude dovádět.
+     F      C    B     F          C          G
+   a loď už bude samej šrám, však šohaj jako květ,
+     C            Dmi7        G        C  Dmi7 G    C Dmi7 G
+   a v lodi malej porcelán mi bude dovádět.
+
+   C          Dmi7         G7         C
+3. Až mi bude šedesát, tak řeknu "johoho",
+         Dmi7  C      F  C     Dmi7        G
+   svý   pádlo chytnu akorát a dám se do toho,
+   C           Dmi7             G         C
+   muži zlíbám smutnou líc, pak vobepluju svět,
+   Dmi7  C    F     C        Dmi7           G
+   až mi bude o pět víc, tak snad se vrátím zpět.
+   F    C      B       F        C         G
+   muži zlíbám smutnou líc, pak vobepluju svět,
+   C          Dmi7           G              C  Dmi7 G    C Dmi7 G
+   až mi bude o pět víc, tak snad se vrátím zpět.
+
+   C          Dmi7           G7         C
+4. Až mi bude dvěstě let, to dávno budu spát,
+      Dmi7  C     F      C         Dmi7       G
+   mý kosti budou práchnivět, však dovolím si přát,
+    C          Dmi7       G         C
+   aby mý tělo studený do rakve nedali,
+     Dmi7     C     F     C     Dmi7      G
+   i s lodí   ať je pohřbený na břehu Sázavy
+    C          Dmi7       G         C
+   aby mý tělo studený do rakve nedali,
+     Dmi7     C     F     C     Dmi7      G       C
+   i s lodí   ať je pohřbený na břehu     Sá  za  vy.
